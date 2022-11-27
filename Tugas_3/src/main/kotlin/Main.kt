a31ef5f838fc6c1b89be289434b9101d1388a31b
Onezero93 <iqbalzabitza93@gmail.com>
2022-11-27 22:25:10 +0700

    Tugas 3 intellij idea

--- Tugas_3/src/main/kotlin/Main.kt
+++ Tugas_3/src/main/kotlin/Main.kt
@@ -1,7 +1,21 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+class bunga (namabunga : String,warnabunga:String? = null ){
+    val namabunga = namabunga
+    val warna = warnabunga
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    fun tam (){
+        println("Nama bunga")
+        var namabunga = readln()
+        println("Nama:" + namabunga)
+        println("Warna bunga")
+        var warna = readln()
+        println("Jenis Bunga")
+        println("Nama Bunga:" + namabunga)
+        println("Warna Bunga:" + warna)
+    }
+}
+fun main(){
+    var mawar = bunga (
+        namabunga = "mawar",
+        warnabunga = "merah")
+    mawar.tam()
 }
